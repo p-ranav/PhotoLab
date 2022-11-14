31bb1a86f87a323c55871651a2ea719ed6665d00
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-11-14 08:36:38 -0800

    Added part files for human segmentation and background removal models

---
+++ FUtils.py
@@ -0,0 +1,34 @@
+import os
+import glob
+
+def split_file(path, chunk_size=50000000):
+    file_number = 1
+    filename = os.path.basename(path)
+    with open(path, 'rb') as f:
+        chunk = f.read(chunk_size)
+        while chunk:
+            with open(os.path.join(os.path.dirname(path), filename + ".part" + str(file_number)), 'wb') as chunk_file:
+                chunk_file.write(chunk)
+            file_number += 1
+            chunk = f.read(chunk_size)
+
+def merge_files(prefix, dirname):
+    '''
+    Each file has a .part<num> suffix
+
+    prefix: The name of the output merged file
+    dirname: Directory containing the split files to be merged
+
+    This function will find all the parts and merge them
+    '''
+    files = glob.glob(os.path.join(dirname, prefix) + "*")
+    files = list(files)
+    files.sort()
+
+    mergedBytes = b''
+    for fn in files:
+        with open(fn, 'rb') as fp:
+            mergedBytes += fp.read()
+
+    with open(os.path.join(dirname, prefix), 'wb') as fp:
+        fp.write(mergedBytes)
